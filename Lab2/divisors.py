c21423861ad3aa7c13f13e71f5d323c23f0a1468
Marah Toubal <maraht13@meet.mit.edu>
2013-10-25 15:50:23 +0300

    divisors

--- Lab2/divisors.py
+++ Lab2/divisors.py
@@ -1,12 +1,13 @@
-n = raw_input("give me a number ")
-numbers = int(n)
-for number in range(numbers) : 
-	print number+1
+def divisors(n):
+    n = int(n)
+    for number in xrange(n) : 
+    	if n%(number+1) == 0:
+		print number+1
 
 
-
-
-
+if __name__ == '__main__':
+	n = raw_input("give me a number ")
+	divisors(n)
 
 
   
